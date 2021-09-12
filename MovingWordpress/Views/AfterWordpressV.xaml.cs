ebd6e4eafeabdebedab4ed51bfb338ce1731e99d
zeikomi552 <68983834+zeikomi552@users.noreply.github.com>
2021-09-12 18:32:35 +0900

    マテリアルデザイン対応

--- MovingWordpress/Views/AfterWordpressV.xaml.cs
+++ MovingWordpress/Views/AfterWordpressV.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,7 +18,7 @@ namespace MovingWordpress.Views
     /// <summary>
     /// AfterWordpressV.xaml の相互作用ロジック
     /// </summary>
-    public partial class AfterWordpressV : Window
+    public partial class AfterWordpressV : MetroWindow
     {
         public AfterWordpressV()
         {
--- MovingWordpress/Views/BeforeWordpressV.xaml.cs
+++ MovingWordpress/Views/BeforeWordpressV.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,7 +18,7 @@ namespace MovingWordpress.Views
     /// <summary>
     /// BeforeWordpressV.xaml の相互作用ロジック
     /// </summary>
-    public partial class BeforeWordpressV : Window
+    public partial class BeforeWordpressV : MetroWindow
     {
         public BeforeWordpressV()
         {
