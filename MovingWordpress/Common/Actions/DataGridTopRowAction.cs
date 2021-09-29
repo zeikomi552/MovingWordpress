b6201415b8d2c8e92dba599bad967c2c6630e6bb
zeikomi552 <68983834+zeikomi552@users.noreply.github.com>
2021-09-29 09:45:53 +0900

    ランダム選択にグリッドを追従させるよう修正

---
+++ MovingWordpress/Common/Actions/DataGridTopRowAction.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MovingWordpress.Common.Actions
+{
+	#region DataGridのスクロールバーを追従させるアクション 
+	/// <summary> 
+	/// DataGridのスクロールバーを追従させるアクション 
+	/// </summary> 
+	public class DataGridTopRowAction : TriggerAction<DataGrid>
+	{
+
+		public static readonly DependencyProperty DataGridTopRowProperty =
+		DependencyProperty.Register("DataGridTopRow", typeof(int), typeof(DataGridTopRowAction), new UIPropertyMetadata());
+
+		public int DataGridTopRow
+		{
+			get { return (int)GetValue(DataGridTopRowProperty); }
+			set { SetValue(DataGridTopRowProperty, value); }
+		}
+
+        protected override void Invoke(object obj)
+		{
+			DataGrid dg = this.AssociatedObject as DataGrid; 
+
+			if (dg != null)
+			{
+				dg.ScrollIntoView(dg.Items[dg.Items.Count - 1]); //scroll to last
+				dg.UpdateLayout();
+				dg.ScrollIntoView(dg.SelectedItem);
+			}
+		}
+	}
+	#endregion
+
+}
